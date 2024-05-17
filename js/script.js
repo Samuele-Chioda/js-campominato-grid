995ef328879724bb1b816d6de19b05d7f18daf72
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-17 20:15:29 +0200

    grid prima parte

--- js/script.js
+++ js/script.js
@@ -4,11 +4,12 @@ for (let index = 0; index < 100; index++) {
     const squareElement = document.createElement("article");
 
     squareElement.classList.add("square");
-    squareElement.append(index);
+
+    squareElement.append(index +1);
     squareElement.addEventListener("click", function(){
         squareElement.classList.add("active");
     });
-    
+
     gridElement.appendChild(squareElement);
 }
 
