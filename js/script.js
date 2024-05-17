883da56afb10bce65e0b3273b83bd7ede0ae53d7
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-17 20:06:56 +0200

    indice

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const gridElement = document.querySelector("div#grid");
 
-for (let index = 0; index < 99; index++) {
+for (let index = 0; index < 100; index++) {
     const squareElement = document.createElement("article");
-    squareElement.classList.add("square");
 
-    gridElement.appendChild(squareElement);
+    squareElement.classList.add("square");
+    squareElement.append(index);
+    squareElement.addEventListener("click", function(){
+        squareElement.classList.add("active");
+    });
     
+    gridElement.appendChild(squareElement);
 }
 
